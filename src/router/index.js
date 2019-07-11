981c46449e5ed87aa97a71dd7cc7185b6b17d02f
zenaJi <1390337439@qq.com>
2019-07-11 20:50:48 +0800

    jizena

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 引入页面组件
+import XhProductDetail from '.././pages/XhProductDetail'
+import XhProductsList from '.././pages/XhProductsList'
+
+
+
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
-
+    {
+      path:'/XhProductDetail',
+      name:'XhProductDetail',
+      component: XhProductDetail
+    },
+    {
+      path:'/XhProductsList',
+      name:'XhProductsList',
+      component: XhProductsList
+    }
   ]
 })
