0f2fd7325bd66b80172f8a9e220180cd7f9a5edf
zybbbbbb <814395061@qq.com>
2019-07-11 19:15:25 +0800

    zyb

--- src/main.js
+++ src/main.js
@@ -28,6 +28,13 @@ Vue.prototype.qs = QS;
 Vue.config.productionTip = false;
 Vue.use(VueAwesomeSwiper);
 Vue.use(vuex);
+
+//vant插件
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+//JQ
+import 'jquery/dist/jquery'
 //使用300ms模块
 fastClick.attach(document.body);
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import XhMessage from '../pages/XhMessage'
+import XhmessagePages from '../pages/XhMessagePages'
+import XhGiftCards from '../pages/XhGiftCards'
+import XhMyStudy from '../pages/XhMyStudy'
 
 Vue.use(Router)
-
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
-
+    {
+      path: '/XhMessage',
+      name: 'XhMessage',
+      component: XhMessage
+    },
+    {
+      path: '/XhmessagePages',
+      name: 'XhmessagePages',
+      component: XhmessagePages
+    },
+    {
+      path: '/XhGiftCards',
+      name: 'XhGiftCards',
+      component: XhGiftCards
+    },
+    {
+      path:'/XhMyStudy',
+      name:'XhMyStudy',
+      component:XhMyStudy
+    }
   ]
 })
