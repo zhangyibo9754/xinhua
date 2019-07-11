0cfacc94fa0075e15373df4485fc780b4092f585
sunhan <17398681335@163.com>
2019-07-11 20:56:21 +0800

    sunhan

--- src/pages/XhNavPages.vue
+++ src/pages/XhNavPages.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="XhNavPages">
+        <!-- Header components中 index 复用 -->
         <Xh-nav-pages-header></Xh-nav-pages-header>
         <div class="XhNavPages-main">
+            <!-- 优惠券页面会员图片 -->
             <Xh-nav-pages-member></Xh-nav-pages-member>
+            <!-- 图书销量排行榜组件 -->
             <Xh-nav-pages-main></Xh-nav-pages-main>
+            <!-- 专题组件 -->
             <Xh-nav-pages-special></Xh-nav-pages-special>
+            
             <Xh-nav-pages-coupon></Xh-nav-pages-coupon>
             <Xh-nav-pages-coupon-soon-start></Xh-nav-pages-coupon-soon-start>
             <Xh-nav-pages-recommen-dation></Xh-nav-pages-recommen-dation>
--- src/pages/XhThemePages.vue
+++ src/pages/XhThemePages.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <Xh-nav-pages-header></Xh-nav-pages-header>  
+        <!-- Header -->
+        <Xh-nav-pages-header></Xh-nav-pages-header> 
         <div class="XhThemePages-main">
-            <!-- <Xh-theme-pages-img></Xh-theme-pages-img> -->
-            <Xh-theme-pages-swiper></Xh-theme-pages-swiper>
-            <Xh-theme-pages-icon></Xh-theme-pages-icon>
+            <!-- 单张图片 -->
             <Xh-theme-pages-img></Xh-theme-pages-img>
+            <!-- 轮播图 -->
+            <Xh-theme-pages-swiper></Xh-theme-pages-swiper>
+            <!-- icon小图标 -->
+            <Xh-theme-pages-icon></Xh-theme-pages-icon>
+            <!-- 单张图片 -->
+            <Xh-theme-pages-img></Xh-theme-pages-img>
+            <!-- 超值套餐组件 -->
             <Xh-theme-pages-set-meal></Xh-theme-pages-set-meal>
         </div>
     </div>
 </template>
 
 <script>
+// 引入组件
 import XhNavPagesHeader from '../components/index/XhNavPagesHeader';
 import XhThemePagesSwiper from '../components/index/XhThemePagesSwiper';
 import XhThemePagesImg from '../components/index/XhThemePagesImg';
@@ -19,6 +26,7 @@ import XhThemePagesIcon from '../components/index/XhThemePagesIcon';
 import XhThemePagesSetMeal from '../components/index/XhThemePagesSetMeal';
     export default {
         name: "XhThemePages",
+        // 注册组件
         components: {
             XhNavPagesHeader,
             XhThemePagesSwiper,
