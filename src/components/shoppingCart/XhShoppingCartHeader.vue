5082e5c7c5b667e741882ae4144502971ca34162
Olny-you2019 <2669986522@qq.com>
2019-07-12 22:15:14 +0800

    liuyao

--- src/components/shoppingCart/XhShoppingCartHeader.vue
+++ src/components/shoppingCart/XhShoppingCartHeader.vue
@@ -1,20 +1,37 @@
 <template>
     <div class="XhShoppingCartHeader">
       <van-row>
-        <van-col class="XhShoppingCartHeaderAndLocation" span="6" offset="9">购物车<van-icon name="location" /></van-col>
+        <van-col class="XhShoppingCartHeaderAndLocation" span="6" offset="9">购物车
+          <van-button type="primary" @click="showPopup" class="XhShoppingCartLocationBox">
+            <van-icon name="location" class="LocationIcon"/>
+          </van-button>
+        </van-col>
         <van-col class="XhShoppingCartHeaderManage" span="4" offset="5">管理</van-col>
       </van-row>
       <van-row>
         <button class="XhShoppingCartHeaderLogin">登录</button>
         <p class="XhShoppingCartHeaderP">登录后商品将合并到您的账号中</p>
       </van-row>
+      <van-popup v-model="show" position="bottom">
+        <van-area :area-list="areaList"/>
+      </van-popup>
     </div>
 </template>
 
 <script>
+  import areaList from "@/assets/js/三级联动"
     export default {
         name: "XhShoppingCartHeader",
+        data() {
+          return {
+            areaList,
+            show: false
+          }
+        },
         methods: {
+          showPopup() {
+            this.show = true;
+          }
         }
     }
 </script>
@@ -31,6 +48,20 @@
     font-size: 0.4rem;
     color:#676767;
     line-height: 1rem;
+  }
+  .XhShoppingCartLocationBox{
+    float: left;
+    width: 0.4rem;
+    height: 0.4rem;
+    background-color: transparent;
+    position: absolute;
+    top: 0.2rem;
+    left: 4rem;
+    border: none;
+  }
+  .LocationIcon{
+    font-size: 0.4rem;
+    color: #676767;
   }
   .XhShoppingCartHeaderManage{
     height: 1rem;
