363e8398c1eff7816a95a4a837d173b3f4f0d2a0
sunhan <17398681335@163.com>
2019-07-13 23:10:49 +0800

    sunhan

---
+++ src/components/index/XhSearchLately.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="XhSearchLately" v-show="history">
+    <p class="XhSearchLately-title-wrapper">
+      <span class="XhSearchLately-title">最近搜索</span>
+      <van-icon class="XhSearchLately-title-icon" name="delete" @click="delHistory"/>
+    </p>
+    <ul class="XhSearchLately-List-box">
+      <li class="XhSearchLately-List-wrapper">
+        <span class="XhSearchLately-List">白鹿原</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "XhSearchLately",
+      data(){
+        return{
+          history: true
+        }
+      },
+      methods: {
+        delHistory(){
+          this.history=false;
+        },
+      }
+    }
+</script>
+
+<style scoped>
+  .XhSearchLately{
+    width: 100%;
+    margin-top:.75rem ;
+    float: left;
+  }
+  .XhSearchLately-title-wrapper{
+    width: 100%;
+    display: flex;
+    float: left;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .XhSearchLately-title{
+    font-size:.3rem ;
+    font-weight:800 ;
+    color: #74787a;
+  }
+  .XhSearchLately-title-icon{
+    font-size: .3rem  !important;
+  }
+  .XhSearchLately-List-box{
+    width: 100%;
+    float: left;
+    margin-top: .22rem;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .XhSearchLately-List{
+    background: #eaebed;
+    font-size: .29rem;
+    margin-right: .23rem;
+    margin-bottom: .23rem;
+    height: .56rem;
+    border-radius: .1rem;
+  }
+  .XhSearchLately-List a{
+    line-height: .56rem;
+    margin:0 .23rem 0 .23rem;
+    font-size: .3rem;
+    color: #999999;
+  }
+</style>
---
+++ src/components/index/XhSearchList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="XhSearchList">
+    <ul class="XhSearchList-box" v-if="title">
+      <li class="XhSearchList-wrapper"  v-for="(item,index) in pendingData" :key="index">
+        <span class="XhSearchList-column">{{item.name}}</span>
+        <van-icon class="XhSearchList-icon" name="exchange" />
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "XhSearchList",
+        props: {
+          title:String,
+          pendingData:Array,
+        },
+    }
+</script>
+
+<style scoped>
+  .XhSearchList-box{
+    width: 7.09rem;
+    margin: .7rem  auto;
+    text-align: left;
+  }
+  .XhSearchList-wrapper{
+    width: 100%;
+    height: .9rem;
+    display: flex;
+    line-height: .9rem;
+    border-bottom: 2px #eeeeee solid;
+    font-size: .3rem;
+    color: #999999;
+  }
+  .XhSearchList-column{
+    display: inline-block;
+    width: 86%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
+  .XhSearchList-icon{
+    font-size: .4rem;
+    line-height: .86rem;
+  }
+</style>
