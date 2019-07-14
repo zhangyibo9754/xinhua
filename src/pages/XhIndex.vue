ef7b320fd79feb62f26d57fa70f10056f2b98b25
hello-world1902 <1026673707@qq.com>
2019-07-14 10:02:27 +0800

    dangyue

--- src/pages/XhIndex.vue
+++ src/pages/XhIndex.vue
@@ -95,7 +95,6 @@
               if (data.data.status === 0) {
                 // 成功时接收首页的数据
                this.indexData=JSON.parse(data.data.datas.designData.body);
-<<<<<<< HEAD
                 //获取首页列表数据
                 var index_list=['body_37','body_38','body_34','body_35','body_36','body_33'];
                 for(let i=0;i<index_list.length;i++){
@@ -104,7 +103,7 @@
                     "Tid":"",
                     "moreLink":"",
                     "indexListBrach":null
-                  }
+                  };
                   for(let j=0;j<this.indexData.length;j++){
                     if(index_list[i]==this.indexData[j].id){
                       index_obj.tlt=this.indexData[j].config.titleText
@@ -146,48 +145,7 @@
                   }
                   this.indexListTheme.push(index_obj)
                   // console.log(this.indexListTheme);
-                }
-=======
-                //获取首页列表数
-               for(var i=0;i<this.indexData.length;i++){
-                 if(this.indexData[i].config){
-                   if(this.indexData[i].config.titleText){
-                     var serviceData,listid;
-                     listid=this.indexData[i].id
-                     serviceData=data.data.datas.serviceData[listid]._DATA_;
-                     var array=[];
-                     var array1=[];
-                     for(var k=0;k<serviceData.length;k++){
-                       if(serviceData[k].name){
-                         array.push({
-                           "goodsId":serviceData[k].id,
-                           // "src":serviceData[k].mainImage,
-                           "src":"https://img1.xinhuashudian.com/images/2019/06/10/e6ea99dc-8b4c-48ea-834b-ae48e26e054b.jpg?x-oss-process=image/resize,m_lfit,limit_0,w_200,h_200",
-                           "tltle":serviceData[k].name,
-                           "price1":serviceData[k].lowPrice/100,
-                           "price2":serviceData[k].highPrice/100
-                         })
-                       }else{
-                         array1.push({
-                           "goodsId":serviceData[k].itemId,
-                           "src":serviceData[k].mainImage,
-                           "tltle":serviceData[k].itemName,
-                           "price1":serviceData[k].lowPrice/100,
-                           "price2":serviceData[k].highPrice/100
-                         })
-                       }
-                     }
-                     this.indexListTheme.push({
-                       "tlt":this.indexData[i].config.titleText,
-                       "Tid":this.indexData[i].id,
-                       "moreLink":this.indexData[i].config.moreLink,
-                       "indexListBrach":array
-                     })
-                   }
-                 }
-               }
-                // console.log(this.indexListTheme);
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
+                };
                 //轮播图
                 this.indexSwipers=this.indexData[6].config.carousel.carouselItems
                 // console.log(this.indexSwipers);
@@ -202,14 +160,9 @@
                 this.indexBanner=this.indexData[19].config.image.src
                 // console.log(this.indexBanner);
 
-                //cs <!--新华优选内容--> 6个
-
+                // <!--新华优选内容--> 6个
                 var all_objs=data.data.datas.serviceData
-<<<<<<< HEAD
                 var body_arrs =JSON.parse(data.data.datas.designData.body)
-=======
-              var body_arrs =JSON.parse(data.data.datas.designData.body)
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
                 var xinHuDingZhi =["body_21","body_27","body_22","body_28","body_23","body_29","body_24","body_30","body_26","body_32","body_25","body_31"]
                 // console.log(all_objs)
                 // console.log(body_arrs)
@@ -223,7 +176,6 @@
                     body_arrs.some(i=>{
                       if(item==i.id){
                         pic1 = i.config.image.src
-
                         return true
                       }
                     })
@@ -235,19 +187,12 @@
                         "price1":(a.lowPrice/100*0.75).toFixed(2),
                         "price2":(a.highPrice/100).toFixed(2)
                       })
-                      // this.indexSelect_cs.push(indexSelect_cs_per)
                     })
                     indexSelect_cs_per.pic=pic1
                     this.indexSelect_cs.push(indexSelect_cs_per)
                   }
                 })
-
-                // console.log(this.indexSelect_cs)
-<<<<<<< HEAD
-                //  <!--作者轮播图-->
-=======
-              //  <!--作者轮播图-->
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
+                //作者轮播图
                 body_arrs.some(item=>{
                   if(item.id=="body_11"){
                     this.indexAuthor_cs=item.config.carousel.carouselItems
@@ -255,11 +200,7 @@
                   }
                 })
                 // <!--图书分类-->
-<<<<<<< HEAD
                 var indexBookList_arrs = ["body_2","body_15","body_5","body_12","body_3","body_14","body_18","body_17","body_8","body_9","body_13","body_10","body_19","body_16"]
-=======
-               var indexBookList_arrs = ["body_2","body_15","body_5","body_12","body_3","body_14","body_18","body_17","body_8","body_9","body_13","body_10","body_19","body_16"]
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
                 var pic2=""
                 indexBookList_arrs.forEach((item,index)=>{
                   var indexBookList_cs_per =  {
@@ -270,7 +211,6 @@
                     body_arrs.some(i=>{
                       if(item==i.id){
                         pic2 = i.config.image.src
-
                         return true
                       }
                     })
@@ -278,13 +218,10 @@
                     all_objs[item]._DATA_.forEach(a=>{
                       indexBookList_cs_per.indexListBrach.push({
                         "src":a.mainImage,
-<<<<<<< HEAD
                         "tltle":item=="body_15"?a.itemName:a.name,
                         "price1":(a.lowPrice/100).toFixed(2),
-=======
                         "tltle":a.name,
                         "price1":a.lowPrice/100,
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
                         // "price2":a.highPrice
                       })
                       // this.indexSelect_cs.push(indexSelect_cs_per)
@@ -293,10 +230,6 @@
                     this.indexBookList_cs.push(indexBookList_cs_per)
                   }
                 })
-<<<<<<< HEAD
-=======
-
->>>>>>> 67d493a0e5af44a2451ae3921fd45a5340ae9409
               } else {
                 // 失败时打印错误信息
                 console.log(data.data.err);
--- src/pages/XhSearch.vue
+++ src/pages/XhSearch.vue
@@ -1,50 +1,10 @@
 	<template>
   <div class="XhSearch">
-<<<<<<< HEAD
     <xh-search-header @changeTitle="changeTitle"></xh-search-header>
     <xh-search-list :pendingData="pendingData" :title="title" @changeTitle="Clear"></xh-search-list>
     <div class="XhSearchFooter" v-if="!title">
       <xh-search-lately></xh-search-lately>
       <xh-search-popular :searchBefore="searchBefore"></xh-search-popular>
-=======
-    <div class="XhSearchHeader">
-      <div class="SearchHeaderCenter">
-        <input v-model="title" class="SearchHeaderCenter-input"    type="text" placeholder="应物兄">
-        <p>
-          <van-icon name="search" v-if="!title" @click="search"/>
-          <van-icon name="cross" v-if="title" @click="delKey"/>
-        </p>
-        <router-link to="">
-          <span @click="$router.back()">取消</span>
-        </router-link>
-      </div>
-    </div>
-    <ul class="SearchBox"  v-if="title">
-      <li v-for="(item,index) in pendingData" :key="index" class="rel">
-        <span>{{item.name}}</span>
-          <i class="abs">—></i>
-      </li>
-    </ul>
-    <div class="XhSearchFooter" v-if="!title">
-      <div class="SearchFooterFooter">
-        <div class="XhSearchLately" v-show="history">
-          <p><span class="XhSearchLately-span" >最近搜索</span>
-            <van-icon name="delete" @click="delHistory"/>
-          </p>
-          <ul>
-            <li><span>白鹿原</span></li>
-          </ul>
-        </div>
-        <div class="XhSearchLately">
-          <p><span class="XhSearchLately-span">热门搜索</span></p>
-          <ul>
-            <li v-for="(before,index) in searchBefore" :key="index">
-              <a href="">{{before}}</a>
-            </li>
-          </ul>
-        </div>
-      </div>
->>>>>>> b4d2ca8c917ba2aaac4711b5234c6ec31e63e02a
     </div>
   </div>
 </template>
@@ -145,104 +105,4 @@
     display: flex;
     flex-direction: column;
   }
-<<<<<<< HEAD
-
-  .XhSearchFooter{
-=======
-	.SearchHeaderCenter-input{
-		display: block;
-		width: 5.5rem;
-		height:.56rem ; 
-		background: #FFFFFF;
-		border: 0 !important;
-		padding-left: .2rem;
-	}
-  .XhSearch .XhSearchHeader{
-    width: 100%;
-    height: .88rem;
-    float: left;
-  }
-	.XhSearchLately-span{
-		font-size:.3rem ;
-		font-weight:800 ;
-		color: #74787a;
-	}
-  .XhSearch .XhSearchHeader .SearchHeaderCenter{
-    width: 7.09rem;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-  }
-  .XhSearch .XhSearchHeader .SearchHeaderCenter p{
-    width: .68rem;
-    height:.56rem;
-    background: #FFFFFF;
-    font-size: .3rem;
-    line-height: .66rem  !important;
-    text-align: center;
-    margin-right: .1rem;
-    color: #999999;
-    font-weight: 900;
-  }
-  .XhSearch .XhSearchHeader .SearchHeaderCenter span{
-    font-size:.27rem ;
-    color:black ;
-  }
-  .XhSearch .XhSearchFooter{
-    width: 100%;
-    float: left;
-  }
-  .XhSearch .XhSearchFooter .SearchFooterFooter{
-    width: 7.09rem;
-    margin: 0 auto;
-  }
-  .XhSearch  .SearchBox{
-    width: 7.09rem;
-    margin: .7rem  auto;
-    text-align: left;
-  }
-  .XhSearch  .SearchBox li{
-    width: 100%;
-    height: .9rem;
-    line-height: .9rem;
-    border-bottom: 2px #eeeeee solid;
-    font-size: .3rem;
-    color: #999999;
-  }
-  .XhSearch  .SearchBox li span{
-    display: inline-block;
-    width: 86%;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 1;
-    -webkit-box-orient: vertical;
-  }
-  .XhSearch  .SearchBox li i{
-    right:0.1rem;
-    top:0;
-  }
-  .XhSearch .XhSearchFooter .SearchFooterFooter .XhSearchLately{
-    width: 100%;
-    margin-top:.75rem ;
-    float: left;
-  }
-  .XhSearch .XhSearchFooter .SearchFooterFooter .XhSearchLately p{
-    width: 100%;
-    display: flex;
-    float: left;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .XhSearch .XhSearchFooter .SearchFooterFooter .XhSearchLately p .van-icon{
-    font-size: .3rem  !important;
-  }
-  .XhSearch .XhSearchFooter .SearchFooterFooter .XhSearchLately ul{
->>>>>>> b4d2ca8c917ba2aaac4711b5234c6ec31e63e02a
-    width: 100%;
-    float: left;
-    padding: 0 .2rem;
-  }
-
 </style>
