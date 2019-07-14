9af797f57214f15034e3cabd9afd370e8d3165de
tomyjessic <2860513024@qq.com>
2019-07-14 22:38:46 +0800

    xx

--- src/components/commons/XhIntoBannerSwiper.vue
+++ src/components/commons/XhIntoBannerSwiper.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div class="title-wrap">
-      <img class="popular-titlePic" :src="item.titlePic" alt="">
+      <img class="popular-titlePic" :src="item.titlePic" alt />
     </div>
-    <div class="swiper-container " id="intoBannerSwiper">
+    <div class="swiper-container" id="intoBannerSwiper">
       <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="(v,vId) in item.swiperSlide" :key="vId">
           <div class="itemList" v-for="(l,lId) in v.itemList" :key="lId">
             <div class="item-wrap">
-              <img class="book-pic" :src="l.img" alt="">
+              <img class="book-pic" :src="l.img" alt />
               <span class="smallItem-name">{{l.name}}</span>
               <span class="book-price">{{l.price}}</span>
             </div>
@@ -22,39 +22,40 @@
 </template>
 
 <script>
-  import Swiper from "swiper/dist/js/swiper";
-  export default {
-    name: "XhIntoBannerSwiper",
-    props: ["item"],
-    mounted() {
-      //在获取完数据后，将swiper放在$nextTick下一个UI帧再初始化。
-      this.$nextTick(() => { // 下一个UI帧再初始化swiper
-        this._initSwiper();
+import Swiper from "swiper";
+export default {
+  name: "XhIntoBannerSwiper",
+  props: ["item"],
+  mounted() {
+    //在获取完数据后，将swiper放在$nextTick下一个UI帧再初始化。
+    this.$nextTick(() => {
+      // 下一个UI帧再初始化swiper
+      this._initSwiper();
+    });
+  },
+  methods: {
+    _initSwiper() {
+      // banner部分的轮播图
+      new Swiper(".swiper-container", {
+        direction: "horizontal", //水平
+        loop: true, //循环
+        observer: true, //动态获取资源时，需要加上这一句
+        // observeParents: true,
+        autoplay: {
+          disableOnInteraction: false
+        },
+        // 分页器
+        pagination: {
+          el: ".swiper-pagination",
+          bulletClass: "my-bullet",
+          bulletActiveClass: "my-bullet-active"
+        }
       });
-    },
-    methods: {
-      _initSwiper() {
-        // banner部分的轮播图
-        new Swiper(".swiper-container", {
-          direction: "horizontal",//水平
-          loop: true, //循环
-          // autoplay: true,
-          // observer: true, //动态获取资源时，需要加上这一句
-          // observeParents: true,
-          // 分页器
-          pagination: {
-            el: ".swiper-pagination",
-            bulletClass: "my-bullet",
-            bulletActiveClass: "my-bullet-active"
-          },
-        });
-      }
     }
-
   }
+};
 </script>
 
 <style>
-  @import "../../assets/css/XhBannerPages.css";
-
+@import "../../assets/css/XhBannerPages.css";
 </style>
