fc8c4f4531c993e06e24b4c32d9152579daa84e6
gongwenhui <2874139279@qq.com>
2019-07-14 18:39:42 +0800

    gongwenhui

--- src/pages/XhMyOrder.vue
+++ src/pages/XhMyOrder.vue
@@ -16,6 +16,33 @@
 			  </div>
 		  </van-tab>
 		  <van-tab title="待付款">
+			 <div class="XhMyorder-box">
+			 				  <div class="XhMyorder-box-one">
+			 					  <div class="XhMyorder-box-one-one">
+			 						  <i class="iconfont">&#xe620;</i>
+			 						  <i>新华书店网上商城自营图书</i>
+			 						  <span>待付款</span>
+			 					  </div>
+			 				<div class="XhMyorder-box-one-two">
+									  <img src="../../static/images/mine/xhimg/4471015-fm.jpg" />
+									  <div class="XhMyorder-box-one-two-span"><span>破冰行动</span></div>
+									  <div class="XhMyorder-box-one-two-span">
+										  <span class="XhMyorder-box-one-two-span-span1">￥68.40</span>
+                                          <span class="XhMyorder-box-one-two-span-span2">x1</span>
+									  </div>
+                                   </div>
+								  <div class="XhMyorder-box-one-three">
+									  <span>共1件商品</span>
+									  <span class="XhMyorder-box-one-three-span">实付款:<i>￥68.40</i></span>
+								  </div>
+								  <div class="XhMyorder-box-one-sie">
+									  <div class="XhMyorder-box-one-sie-one"><span>支付</span></div>
+									  <div class="XhMyorder-box-one-sie-two"><span>取消订单</span></div>
+								  </div>
+			 				  </div>
+							  
+			 </div>
+			 
 			  <div class="XhMyOrder-Bigbox">
 			  	<img src="../../static/images/mine/xhimg/xz.png">
 			  	<h6>暂无相关订单</h6>
@@ -23,6 +50,7 @@
 			  </div>
 		  </van-tab>
 		  <van-tab title="待发货">
+			  
 			  <div class="XhMyOrder-Bigbox">
 			  	<img src="../../static/images/mine/xhimg/xz.png">
 			  	<h6>暂无相关订单</h6>
@@ -30,6 +58,7 @@
 			  </div>
 		  </van-tab>
 		  <van-tab title="待收货">
+			  
 			  <div class="XhMyOrder-Bigbox">
 			  	<img src="../../static/images/mine/xhimg/xz.png">
 			  	<h6>暂无相关订单</h6>
@@ -83,13 +112,13 @@
 }
 .XhMyOrder-header img{
 	float: left;
-	width: .5rem;
-	height: .7rem;
-	margin-top: .3rem;
+	width: .3rem;
+	height: .5rem;
+	margin-top: .4rem;
 	margin-left: .2rem;
 }
 .XhMyOrder-header span{
-	font-size: 20px;
+	font-size: 16px;
 	line-height: 1.31rem;
 	color: #717171;
 	text-align: center;
@@ -126,5 +155,118 @@
 	font-size: 20px;
 	color: white;
 	line-height: 1rem;
+}
+.XhMyorder-box{
+	width: 100%;
+}
+.XhMyorder-box-one{
+	width: 7rem;
+	height: 5rem;
+	background: #fff;
+	margin: .3rem auto;
+	border-radius: 5px;
+}
+.XhMyorder-box-one-one{
+	width: 100%;
+	height: 1rem;
+	border-bottom: 1px solid #cfcfcf;
+}
+.XhMyorder-box-one-one i{
+	float: left;
+	line-height: 1rem;
+	margin-left: .2rem;
+	color: #909395;
+	font-size: 16px;
+	}
+.XhMyorder-box-one-one span{
+	float: right;
+	color:#fcdf8d;
+	font-size: 16px;
+	line-height: 1rem;
+	margin-right: .1rem;
+}
+.XhMyorder-box-one-two{
+	width: 100%;
+	height: 2rem;
+	border-bottom: 1px solid #cfcfcf;
+}
+.XhMyorder-box-one-two img{
+	width: 2rem;
+	height: 1.8rem;
+	float: left;
+	margin-top: .1rem;
+}
+.XhMyorder-box-one-two-span span{
+	float: left;
+	font-size: 16px;
+	line-height: 1rem;
+	margin-left: .2rem;
+	}
+	.XhMyorder-box-one-two i{
+		float: left;
+		line-height: 3rem;
+		margin-right: 1rem;
+	}
+.XhMyorder-box-one-two-span{
+	width: 5rem;
+	height: 1rem;
+	float: right;
+}	
+.XhMyorder-box-one-two-span-span1{
+	color: red;
+}
+.XhMyorder-box-one-two-span-span2{
+	float: right !important;
+	color: #999999;
+	margin-right: .2rem;
+}
+.XhMyorder-box-one-three{
+	width: 100%;
+	height: 1rem;
+	border-bottom: 1px solid #cfcfcf;
+}
+.XhMyorder-box-one-three span{
+	float: left;
+	font-size: 16px;
+	line-height: 1rem;
+	color: #909395;
+	margin-left: .2rem;
+}
+.XhMyorder-box-one-three-span{
+	float: right !important;
+	margin-right: .2rem;
+}
+.XhMyorder-box-one-three i{
+	color: #000 !important;
+}
+.XhMyorder-box-one-sie{
+	width: 100%;
+	height: 1rem;
+	display: flex;
+}
+.XhMyorder-box-one-sie-one{
+	width: 2rem;
+	height: .8rem;
+	background: red;
+	margin-top: .1rem;
+	margin-left: 2rem;
+}
+.XhMyorder-box-one-sie-one span{
+	font-size: 16px;
+	color: white;
+	line-height: .8rem;
+	text-align: center;
+}
+.XhMyorder-box-one-sie-two{
+	width: 2rem;
+	height: .8rem;
+	margin-top: .1rem;
+	border: 1px solid #cfcfcf;
+	margin-left: .5rem;
+}
+.XhMyorder-box-one-sie-two span{
+	font-size: 16px;
+	line-height: .8rem;
+	text-align: center;
 }
 </style>
--- src/pages/XhPersonalMessage.vue
+++ src/pages/XhPersonalMessage.vue
@@ -4,46 +4,221 @@
         <span class="iconfont icon-fanhui" @click="$router.back()"></span>
         <span class="set-cs">个人资料</span>
       </div>
-      <div class="main-cs">
+      <div class="main-cs" >
         <div class="pic-cs">
-          <span>头像</span>
+          <span @click="on">头像</span>
           <div>
-            <img src="../../static/cs/个人资料_03.png">
-            <i class="iconfont icon-jiantouyou"></i>
+            <img src="../../static/cs/个人资料_03.png" @click="on" >
+            <i class="iconfont icon-jiantouyou"  @click="on"></i>
           </div>
+					<van-popup v-model="pushow" :style="{ height: '30%', width:'80%' }">
+						<div class="main-cs-one">
+							<div class="main-cs-one-one">
+								<h3>请选择</h3>
+						<a href=""><h3>拍照</h3></a>	
+							<a href="">	<h3>相册</h3></a>
+							</div>
+			         <div class="main-cs-one-two"><span>取消</span></div>
+						</div>
+					</van-popup>
         </div>
+					
         <div class="name-cs">
-          <span>昵称</span>
+          <span  @click="nc">昵称</span>
           <div>
-            <span>孔式</span>
-            <i class="iconfont icon-jiantouyou"></i>
+            <span  @click="nc">孔式</span>
+            <i class="iconfont icon-jiantouyou"  @click="nc"></i>
           </div>
+					<van-popup v-model="nichen" position="top" :style="{ height: '60%', width:'100%', }">
+						<div class="name-cs-one">
+							<div class="name-cs-one-header">
+								<img src="../../static/images/mine/xhimg/jtz.png" @click="$router.back(-1)">
+								<span>浏览记录</span>
+							</div>
+							<div class="name-cs-one-nav">
+								<span>昵称</span></br>
+								<input type="text" value="孔式"/>
+							</div>
+							<div class="name-cs-two"><span>保存</span></div>
+						</div>
+					</van-popup>
         </div>
+				
         <div class="birthday-cs">
-          <span>生日</span>
+          <span @click="er">生日</span>
           <div>
-            <span>1994-08-06</span>
-            <i class="iconfont icon-jiantouyou"></i>
+            <span @click="er">1994-08-06</span>
+            <i class="iconfont icon-jiantouyou" @click="er"></i>
           </div>
+					<van-popup v-model="time" position="bottom" :style="{ height: '30%'}">
+						<div class="birthday-cs-one">
+							<van-datetime-picker v-model="currentDate" type="date" :min-date="minDate"/>
+						</div>
+					</van-popup>
         </div>
+				
         <div class="sex-cs" >
-          <span>性别</span>
+          <span  @click="showPopup">性别</span>
           <div>
-            <span>保密</span>
-            <i class="iconfont icon-jiantouyou"></i>
+            <span @click="showPopup">保密</span>
+            <i class="iconfont icon-jiantouyou" @click="showPopup"></i>
           </div>
+					<van-popup v-model="show" position="bottom" :style="{ height: '30%'}">
+						<div class="sec-cs-one">
+							<div class="sec-cs-one-one"><a><span>男</span></a></div>
+							<div class="sec-cs-one-one"><a><span>女</span></a></div>
+							<div class="sec-cs-one-one"><a><span>保密</span></a></div>
+							<div class="sec-cs-one-two"><a><span>取消</span></a></div>
+						</div>
+						
+					</van-popup>
         </div>
       </div>
     </div>
 </template>
 
 <script>
+	import { Popup } from 'vant';
+	import { DatetimePicker } from 'vant';
     export default {
-        name: "XhPersonalMessage"
+        name: "XhPersonalMessage",
+			data() {
+    return {
+      show: false,
+			pushow:false,
+			time:false,
+			nichen:false,
+			currentDate: new Date()
+	    }
+  },
+
+  methods: {
+    showPopup() {
+      this.show = true;
+    },
+		on(){
+			this.pushow=true;
+		},
+		er(){
+			this.time=true;
+		},
+		nc(){
+			this.nichen=true;
+		}
+  }
+  
+  
     }
 </script>
 
 <style scoped>
+	.name-cs-one{
+		width: 100%;
+		height: 100%;
+		background: #f7f7f7;
+	}
+	.name-cs-one-header{
+		width: 100%;
+		height: 1.31rem;
+	}
+	.name-cs-one-header img{
+		float: left;
+		width: .3rem;
+		height: .5rem;
+		margin-top: .4rem;
+		margin-left: .2rem;
+	}
+	.name-cs-one-header span{
+		font-size: 16px;
+		line-height: 1.31rem;
+		color: #717171;
+		text-align: center;
+	}
+	.name-cs-one-nav{
+		width: 95%;
+		height: 1.5rem;
+		background: white;
+		text-align: left;
+		margin: 0 auto;
+	}
+	.name-cs-one-nav span{
+		color: #CFCFCF;
+		font-size: 12px;
+		margin-left: .2rem;
+	}
+	.name-cs-one-nav input{
+		width: 95%;
+		height: 1.1rem;
+		border: none;
+		font-size: 18px;
+		margin-left: .2rem;
+	}
+	.name-cs-two{
+		width: 95%;
+		margin: 0 auto;
+		height: 1rem;
+		background: red;
+		margin-top: .5rem;
+	}
+	.name-cs-two span{
+		font-size: 16px;
+		line-height: 1rem;
+		text-align: center;
+		color: white;
+	}
+	.birthday-cs-one{
+		width: 100%;
+		height: 100%;
+		background: red;
+	}
+	.main-cs-one{
+		width:100%;
+		height: 100%;
+	}
+	.main-cs-one-one{
+		width: 100%;
+		height: 3rem;
+	}
+	.main-cs-one-one h3{
+		text-align: left;
+		margin-left: .3rem;
+		line-height: 1rem;
+		color: #000;
+	}
+	.main-cs-one-two{
+		width: 100%;
+		height: 1rem;
+	}
+	.main-cs-one-two span{
+		float: right;
+		line-height: 1rem;
+		color:#0077AA;
+		margin-right: .3rem;
+	}
+	.sec-cs-one{
+		width: 100%;
+		height: 4rem;
+	}
+	.sec-cs-one-one{
+		width: 100%;
+		height: 1rem;
+		border-bottom: 1px solid #BBBBBB;
+	}
+	.sec-cs-one-one span{
+		font-size: 16px;
+		text-align: center;
+		line-height: 1rem;
+		color: #000;
+	}
+	.sec-cs-one-two{
+		width: 100%;
+		height: 1rem;
+	}
+	.sec-cs-one-two span{
+		font-size: 16px;
+		text-align: center;
+	line-height: 1rem;
+	}
   .XhPersonalMessage-cs{
     background-color: #f7f7f7;
     height: 100%;
@@ -120,4 +295,5 @@
   .XhPersonalMessage-cs .main-cs .sex-cs>div>i{
     margin-left: 0.26rem
   }
+	
 </style>
